d68a53c055ce35a18fe4442a6f5948ade0b9b6da
thuvarahan97 <thuvarahan97@gmail.com>
2020-03-07 22:55:12 +0530

    UI Design for HomePage and ScorePage

--- src/app/pages/scores/scores.page.ts
+++ src/app/pages/scores/scores.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastController, LoadingController, AlertController, NavController } from "@ionic/angular";
+import { AccessProviders } from "../../providers/access-providers";
+import { Storage } from "@ionic/storage";
 
 @Component({
   selector: 'app-scores',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoresPage implements OnInit {
 
-  constructor() { }
+  private score1: Number = 0;
+  private score2: Number = 0;
+
+  constructor(
+    private router: Router,
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+    private navCtrl: NavController,
+    private accsPrvds: AccessProviders,
+    private storage: Storage
+  ) { }
 
   ngOnInit() {
   }
+
 
 }
