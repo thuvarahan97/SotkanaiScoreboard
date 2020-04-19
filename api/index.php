9c469a7bfe2fd506f409685926364d86b4ced461
thuvarahan97 <thuvarahan97@gmail.com>
2020-04-19 19:32:45 +0530

    Finalized admin index page.

--- api/index.php
+++ api/index.php
@@ -1,8 +1,6 @@
 <?php 
 
 include_once 'config.php';
-session_start();
-date_default_timezone_set("Asia/Colombo");
 
 if(isset($_SESSION['user_id'])){
     echo "<script>location.replace('competitions.php')</script>";
